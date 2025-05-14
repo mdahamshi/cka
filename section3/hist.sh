eedb9b36e83e6c2bb62cee318ec17b9f26e55914
Mohammad <mohammad.dahamshi@gmail.com>
2025-05-14 15:10:54 +0300

    start section 5

--- section3/hist.sh
+++ section3/hist.sh
@@ -4,3 +4,29 @@ k taint nodes node-name key=value:NoSchedule|NoExecute|PreferNoSchedule
 k taint nodes node01 app-blue:NoSchedule
 k taint node node01 spray=mortein:NoSchedule
 
+k get priorityclasses
+
+kube-apiserver -h | grep enable-admission-plugins
+kubectl exec kube-apiserver-controlplane -n kube-system  -- kube-apiserver -h | grep enable-admission-plugins
+
+k describe pod -n kube-system  kube-apiserver-controlplane 
+crictl ps | grep kube-apiserver
+
+journalctl -u kubelet -f
+
+
+
+kubectl create secret tls webhook-server-tls \
+  --cert=/root/keys/webhook-server-tls.crt \
+  --key=/root/keys/webhook-server-tls.key \
+  -n webhook-demo
+
+kubectl get secret webhook-server-tls -n webhook-demo
+
+kubectl describe secret webhook-server-tls -n webhook-demo
+
+
+k top node
+k top pod
+
+k logs -f nginx-6c4b5f7c8d-2q9gk
